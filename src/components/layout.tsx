dbcb37cc4f7a9119dbc1e1ecaaf08e2f86035eea
Olivier Lardenois <olardenois@gmail.com>
2023-01-23 11:39:39 +0100

    :fire: - Remove default code from ts files

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,47 +5,20 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from "react";
 
-import Header from "./header"
-import "./layout.css"
+type LayoutProps = {
+  children?: React.ReactNode;
+};
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = ({ children }: LayoutProps) => {
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <header>header</header>
+      <main>{children}</main>
+      <footer>footer</footer>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
