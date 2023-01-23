c9bfa23848dee372a0a962ec726ce8cec0753dd4
Olivier Lardenois <olardenois@gmail.com>
2023-01-23 13:53:30 +0100

    :lipstick: - Add logo

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,27 +2,27 @@ import { GatsbyConfig } from "gatsby";
 
 const gatsbyConfig: GatsbyConfig = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: "Champagne Chaléroux-Ghys",
+    description: "",
+    author: "",
+    siteUrl: "https://champagnechalerouxghys.fr/",
   },
   graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-postcss",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/locales`,
-        name: `locale`,
+        name: "locale",
       },
     },
     {
-      resolve: `gatsby-plugin-react-i18next`,
+      resolve: "gatsby-plugin-react-i18next",
       options: {
-        languages: [`en`, `fr`],
-        defaultLanguage: `fr`,
-        siteUrl: `https://example.com`,
+        languages: ["en", "fr"],
+        defaultLanguage: "fr",
+        siteUrl: "https://champagnechalerouxghys.fr/",
         // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
         trailingSlash: "always",
         i18nextOptions: {
@@ -47,15 +47,15 @@ const gatsbyConfig: GatsbyConfig = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "Champagne Chaléroux-Ghys",
+        short_name: "Chaléroux-Ghys`",
         start_url: "/",
         background_color: "#663399",
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: "#663399",
         display: "minimal-ui",
-        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+        icon: "src/images/logo.png",
       },
     },
   ],
