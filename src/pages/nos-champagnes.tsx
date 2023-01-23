f1241cd9adceea8d938106c21ab093c4885f37f1
Olivier Lardenois <olardenois@gmail.com>
2023-01-23 22:29:22 +0100

    :construction: - Start implementing productPage

--- src/pages/nos-champagnes.tsx
+++ src/pages/nos-champagnes.tsx
@@ -1,4 +1,5 @@
 import { graphql } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 import * as React from "react";
 
@@ -10,7 +11,41 @@ export default function ProductsPage() {
 
   return (
     <Layout>
-      <h1>ProductsPage</h1>
+      <section className="py-20 space-y-20 max-w-6xl mx-auto">
+        <article className="flex bg-mainBlack rounded-xl text-white">
+          <StaticImage
+            src="../images/brut.jpg"
+            alt=""
+            className="shrink-0 w-80 h-96 rounded-l-xl"
+          />
+          <div className="space-y-6 p-8">
+            <h2>{t("productsPage.champagnes.brut.title")}</h2>
+            <p>{t("productsPage.champagnes.brut.text")}</p>
+          </div>
+        </article>
+        <article className="flex bg-mainBlack rounded-xl text-white">
+          <div className="space-y-6 p-8">
+            <h2>{t("productsPage.champagnes.millesime.title")}</h2>
+            <p>{t("productsPage.champagnes.millesime.text")}</p>
+          </div>
+          <StaticImage
+            src="../images/millesime.jpg"
+            alt=""
+            className="shrink-0 w-80 h-96 rounded-r-xl"
+          />
+        </article>
+        <article className="flex bg-mainBlack rounded-xl text-white">
+          <StaticImage
+            src="../images/rose.jpg"
+            alt=""
+            className="shrink-0 w-80 h-96 rounded-l-xl"
+          />
+          <div className="space-y-6 p-8">
+            <h2>{t("productsPage.champagnes.rose.title")}</h2>
+            <p>{t("productsPage.champagnes.rose.text")}</p>
+          </div>
+        </article>
+      </section>
     </Layout>
   );
 }
