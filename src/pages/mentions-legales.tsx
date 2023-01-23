86c9f4bfd9eeef5636e5c6b05964ffbfcffeb736
Olivier Lardenois <olardenois@gmail.com>
2023-01-23 23:13:59 +0100

    :sparkles: - Implements legalPage

--- src/pages/mentions-legales.tsx
+++ src/pages/mentions-legales.tsx
@@ -10,7 +10,34 @@ export default function LegalPage() {
 
   return (
     <Layout>
-      <h1>LegalPage</h1>
+      <section className="py-20 space-y-20 max-w-6xl mx-auto">
+        <article className="space-y-8">
+          <h2>{t("legalPage.editor.title")}</h2>
+          <p>
+            {t("legalPage.editor.address")}
+            <br />
+            SIRET : 38136642600017
+            <br />
+            Code APE : 0121Z TVA : FR04381366426
+            <br />
+            {t("legalPage.editor.phoneNumber")}
+            <br />
+            {t("legalPage.editor.mail")}
+            <br />
+            {t("legalPage.editor.chief")}
+          </p>
+        </article>
+        <article className="space-y-8">
+          <h2>{t("legalPage.hosting.title")}</h2>
+          <p>
+            Vercel Inc.
+            <br />
+            340 S Lemon Ave #4133
+            <br />
+            Walnut, CA 91789
+          </p>
+        </article>
+      </section>
     </Layout>
   );
 }
