c9231d6ba4b35ade012de96b85a4c2623bbd19bd
Olivier Lardenois <olardenois@gmail.com>
2023-01-23 11:35:18 +0100

    :wrench: - TS-ify the project

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,13 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
+import { GatsbyConfig } from "gatsby";
 
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
-module.exports = {
+export const onCreateNode: GatsbyConfig = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -40,4 +33,4 @@ module.exports = {
       },
     },
   ],
-}
+};
