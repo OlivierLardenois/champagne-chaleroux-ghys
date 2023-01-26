1487304f323cfd3f437efe96399bb89849ccf11a
Olivier Lardenois <olardenois@gmail.com>
2023-01-26 16:23:21 +0100

    :zap: - Remove extrabold import

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -4,7 +4,6 @@ import "@fontsource/open-sans"; // Defaults to weight 400 with normal variant
 import "@fontsource/open-sans/500.css";
 import "@fontsource/open-sans/600.css";
 import "@fontsource/open-sans/700.css";
-import "@fontsource/open-sans/800.css";
 
 import React from "react";
 import { ParallaxProvider } from "react-scroll-parallax";
