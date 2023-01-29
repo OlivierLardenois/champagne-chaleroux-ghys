20b7787e9051ce4b335d71144f265f9d127818c8
Olivier Lardenois <olardenois@gmail.com>
2023-01-29 12:54:43 +0100

    :recycle: - Create ColumnWrapper component

--- src/pages/galerie.tsx
+++ src/pages/galerie.tsx
@@ -5,12 +5,47 @@ import * as React from "react";
 
 import Layout from "../components/layout";
 
+type ColumnWrapperProps = {
+  images: Queries.GalleryPageQuery["galleryImages"]["nodes"];
+  columnNumber: number;
+  columnOrder: number;
+};
+
+const ColumnWrapper = ({
+  images,
+  columnNumber,
+  columnOrder,
+}: ColumnWrapperProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex flex-col space-y-2">
+      {images.map((image, i) => {
+        if (i % columnNumber === columnOrder) return null;
+
+        const gatsbyImageData = getImage(
+          image.childImageSharp?.gatsbyImageData ?? null
+        );
+        if (!gatsbyImageData) return null;
+
+        // https://tailwindcss.com/docs/content-configuration#dynamic-class-names
+        return (
+          <GatsbyImage
+            image={gatsbyImageData}
+            alt={t(`galleryPage.alts.${i}`)}
+            className="h-1/5"
+            key={i}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
 export default function GalleryPage({
   data,
   location,
 }: PageProps<Queries.GalleryPageQuery>) {
-  const { t } = useTranslation();
-
   const images = [...data.galleryImages.nodes].sort(function (a, b) {
     return a.name.localeCompare(b.name, undefined, {
       numeric: true,
@@ -21,45 +56,8 @@ export default function GalleryPage({
   return (
     <Layout pageKey="galleryPage" pathname={location.pathname}>
       <section className="flex flex-col sm:flex-row sm:space-x-2 space-y-2 sm:space-y-0 sm:py-2 sm:px-6 max-w-6xl mx-auto">
-        <div className="flex flex-col space-y-2">
-          {images.map((image, i) => {
-            if (i % 2 === 1) return null;
-
-            const gatsbyImageData = getImage(
-              image.childImageSharp?.gatsbyImageData ?? null
-            );
-            if (!gatsbyImageData) return null;
-
-            // https://tailwindcss.com/docs/content-configuration#dynamic-class-names
-            return (
-              <GatsbyImage
-                image={gatsbyImageData}
-                alt={t(`galleryPage.alts.${i}`)}
-                className="h-1/5"
-                key={i}
-              />
-            );
-          })}
-        </div>
-        <div className="flex flex-col space-y-2">
-          {images.map((image, i) => {
-            if (i % 2 === 0) return null;
-
-            const gatsbyImageData = getImage(
-              image.childImageSharp?.gatsbyImageData ?? null
-            );
-            if (!gatsbyImageData) return null;
-
-            return (
-              <GatsbyImage
-                image={gatsbyImageData}
-                alt={t(`galleryPage.alts.${i}`)}
-                className="h-1/5"
-                key={i}
-              />
-            );
-          })}
-        </div>
+        <ColumnWrapper images={images} columnNumber={2} columnOrder={1} />
+        <ColumnWrapper images={images} columnNumber={2} columnOrder={0} />
       </section>
     </Layout>
   );
